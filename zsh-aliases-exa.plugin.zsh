15b1e3490a3b705d44307190d1514286531a4713
Bl0xhead <Bl0xhead@protonmail.com>
2022-03-29 11:54:38 +0100

    Fixing --git

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -4,17 +4,17 @@
 # Created:  2019-04-12 19:07:28
 # Author:   Darrin Tisdale
 # -----
-# Modified: 2019-05-14 23:18:24
-# Editor:   Darrin Tisdale
+# Modified: 2022-03-29 23:18:24
+# Editor:   Bl0xhead
 #
 
 # general use
 alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias l='exa -lbF '                                               # list, size, type, git
+alias ll='exa -lbGF '                                             # long list
+alias llm='exa -lbGF --sort=modified'                            # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --color-scale'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --color-scale' # all + extended list
 
 # speciality views
 alias lS='exa -1'			                                                  # one column, just names
